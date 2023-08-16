8f11b6829e21159e79028569ef989a07328d9ebb
yeyou <527084408@qq.com>
2023-08-16 09:29:20 +0800

    Add files via upload

---
+++ yx/rrsp.js
@@ -0,0 +1,11 @@
+/******************************
+*人人美剧解锁永久VIP
+*主机名api.hujuvod.com
+*TG频道∶https://t.me/iosrxwy/
+*公众号∶iOS宝藏
+*/
+
+var body = $response.body.replace(/expiredTime":\d+/g,'expiredTime":253392455349000')
+.replace(/valid":\w+/g,'valid":true')
+.replace(/endTime":".*?"/g,'endTime":"9999-11-15 11:55:06"')
+$done({ body });
